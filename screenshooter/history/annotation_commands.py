32c407cae565797c8c1dfd852caaa947b6294d64
moleval <moleval@yandex.ru>
2026-09-23 21:20:58 +0500

    Fix curved geometry history dispatch

--- screenshooter/history/annotation_commands.py
+++ screenshooter/history/annotation_commands.py
@@ -2,9 +2,6 @@
 
 from PyQt5.QtWidgets import QUndoCommand
 
-from ..items import LineItem, WavyLineItem, ArrowItem, CurvedArrowItem, DimensionItem
-
-
 class ResizeAnnotationCommand(QUndoCommand):
     """Атомарное изменение геометрии аннотации через её нативный API."""
 
@@ -26,23 +23,20 @@ class ResizeAnnotationCommand(QUndoCommand):
                 self.item.set_curve(start, end, ctrl)
             else:
                 start, end = geometry
-            # Используем нативный API конкретного line-like item.
-            # Проверка по API надёжнее isinstance для QGraphicsItem/SIP-обёрток:
-            # LineItem наследуется от QGraphicsLineItem, остальные типы имеют
-            # собственные методы set_points/set_line/setRect.
-            if hasattr(self.item, "setLine"):
-                self.item.setLine(start.x(), start.y(), end.x(), end.y())
-            elif hasattr(self.item, "set_points"):
-                self.item.set_points(start.x(), start.y(), end.x(), end.y())
-            elif hasattr(self.item, "set_line"):
-                self.item.set_line(start, end)
-            elif hasattr(self.item, "setRect"):
-                self.item.setRect(start, end)
-            else:
-                raise TypeError(
-                    f"Unsupported annotation geometry: "
-                    f"{type(self.item).__module__}.{type(self.item).__name__}"
-                )
+                # Используем нативный API конкретного line-like item.
+                if hasattr(self.item, "setLine"):
+                    self.item.setLine(start.x(), start.y(), end.x(), end.y())
+                elif hasattr(self.item, "set_points"):
+                    self.item.set_points(start.x(), start.y(), end.x(), end.y())
+                elif hasattr(self.item, "set_line"):
+                    self.item.set_line(start, end)
+                elif hasattr(self.item, "setRect"):
+                    self.item.setRect(start, end)
+                else:
+                    raise TypeError(
+                        f"Unsupported annotation geometry: "
+                        f"{type(self.item).__module__}.{type(self.item).__name__}"
+                    )
         elif rect is not None:
             self.item.setRect(rect)
 
