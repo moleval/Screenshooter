9e0860c00dba4367105bd23cf27d7368c6c383b4
moleval <moleval@yandex.ru>
2026-09-23 11:03:16 +0500

    Шаг 3: сохранить позицию аннотации в Undo/Redo resize

--- screenshooter/history/annotation_commands.py
+++ screenshooter/history/annotation_commands.py
@@ -9,14 +9,21 @@ from PyQt5.QtWidgets import QUndoCommand
 class ResizeAnnotationCommand(QUndoCommand):
     """Команда изменения геометрии аннотации."""
 
-    def __init__(self, item, old_rect, new_rect):
+    def __init__(self, item, old_rect, new_rect, old_pos=None, new_pos=None):
         super().__init__("Изменить размер аннотации")
         self.item = item
         self.old_rect = old_rect
         self.new_rect = new_rect
+        self.old_pos = old_pos
+        self.new_pos = new_pos
+
+    def _apply(self, rect, pos):
+        self.item.setRect(rect)
+        if pos is not None:
+            self.item.setPos(pos)
 
     def redo(self):
-        self.item.setRect(self.new_rect)
+        self._apply(self.new_rect, self.new_pos)
 
     def undo(self):
-        self.item.setRect(self.old_rect)
+        self._apply(self.old_rect, self.old_pos)
