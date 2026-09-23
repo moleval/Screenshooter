f4ea2f17f0853c47a639474ade7029810067e205
moleval <moleval@yandex.ru>
2026-09-23 06:25:10 +0500

    Сохранять центр подложки при повороте

--- screenshooter/history/background_commands.py
+++ screenshooter/history/background_commands.py
@@ -17,7 +17,8 @@ class CropCommand(QUndoCommand):
 
     def __init__(self, scene, background_item, old_pixmap, new_pixmap, items_to_remove,
                  blur_controller=None, crop_rect=None,
-                 items_to_shift=None, old_positions=None, new_positions=None):
+                 items_to_shift=None, old_positions=None, new_positions=None,
+                 background_pos=None):
         super().__init__("Обрезка")
         self.scene = scene
         self.background_item = background_item
@@ -31,6 +32,8 @@ class CropCommand(QUndoCommand):
         self.items_to_shift = items_to_shift or []
         self.old_positions = old_positions or []
         self.new_positions = new_positions or []
+        self.background_pos = background_pos
+        self.new_background_pos = new_background_pos
 
         if self.blur_controller is not None:
             self.blur_state = self.blur_controller._get_blur_state()
@@ -47,6 +50,8 @@ class CropCommand(QUndoCommand):
             item.setPos(new_pos)
 
         self.background_item.setPixmap(self.new_pixmap)
+        if self.background_pos is not None:
+            self.background_item.setPos(self.background_pos)
         self.background_item.update()
 
         if self.blur_controller is not None and self.crop_rect is not None:
@@ -84,7 +89,8 @@ class RotateCommand(QUndoCommand):
     """
 
     def __init__(self, scene, background_item, old_pixmap, new_pixmap, items_to_remove,
-                 blur_controller=None, background_pos=None):
+                 blur_controller=None, background_pos=None,
+                 new_background_pos=None):
         super().__init__("Поворот")
         self.scene = scene
         self.background_item = background_item
@@ -107,7 +113,9 @@ class RotateCommand(QUndoCommand):
                 self.removed_items.append(item)
 
         self.background_item.setPixmap(self.new_pixmap)
-        if self.background_pos is not None:
+        if self.new_background_pos is not None:
+            self.background_item.setPos(self.new_background_pos)
+        elif self.background_pos is not None:
             self.background_item.setPos(self.background_pos)
         self.background_item.update()
 
