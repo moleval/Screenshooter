e4b51030f2c2693c997a9711e656a005afd98510
moleval <moleval@yandex.ru>
2026-09-23 20:45:55 +0500

    feat: support line annotation resize history

--- screenshooter/history/annotation_commands.py
+++ screenshooter/history/annotation_commands.py
@@ -1,29 +1,41 @@
-"""
-Модуль: history/annotation_commands.py
-Описание: команды Undo/Redo для изменения геометрии аннотаций.
-"""
+"""Команды Undo/Redo для изменения геометрии аннотаций."""
 
 from PyQt5.QtWidgets import QUndoCommand
 
 
 class ResizeAnnotationCommand(QUndoCommand):
-    """Команда изменения геометрии аннотации."""
+    """Атомарное изменение геометрии аннотации через её нативный API."""
 
-    def __init__(self, item, old_rect, new_rect, old_pos=None, new_pos=None):
+    def __init__(self, item, old_rect=None, new_rect=None, old_pos=None,
+                 new_pos=None, old_geometry=None, new_geometry=None):
         super().__init__("Изменить размер аннотации")
         self.item = item
         self.old_rect = old_rect
         self.new_rect = new_rect
         self.old_pos = old_pos
         self.new_pos = new_pos
+        self.old_geometry = old_geometry
+        self.new_geometry = new_geometry
 
-    def _apply(self, rect, pos):
-        self.item.setRect(rect)
+    def _apply(self, rect, pos, geometry):
+        if geometry is not None:
+            start, end = geometry
+            if hasattr(self.item, "set_line"):
+                self.item.set_line(start, end)
+            elif hasattr(self.item, "set_points"):
+                self.item.set_points(start.x(), start.y(), end.x(), end.y())
+            elif self.item.__class__.__name__ == "DimensionItem":
+                self.item.setRect(start, end)
+            else:
+                raise TypeError("Unsupported annotation geometry")
+        elif rect is not None:
+            self.item.setRect(rect)
+
         if pos is not None:
             self.item.setPos(pos)
 
     def redo(self):
-        self._apply(self.new_rect, self.new_pos)
+        self._apply(self.new_rect, self.new_pos, self.new_geometry)
 
     def undo(self):
-        self._apply(self.old_rect, self.old_pos)
+        self._apply(self.old_rect, self.old_pos, self.old_geometry)
