8deb928fcb66db9ce1275ec0f1775c69006ef99a
moleval <moleval@yandex.ru>
2026-09-24 17:31:57 +0500

    Add trim-fields image processing tests

--- screenshooter/tests/test_image_processing.py
+++ screenshooter/tests/test_image_processing.py
@@ -2,7 +2,7 @@
 from PyQt5.QtCore import QRectF
 from PyQt5.QtGui import QPixmap, QColor
 
-from screenshooter.image_processing import crop_pixmap, rotate_pixmap, blur_region
+from screenshooter.image_processing import crop_pixmap, rotate_pixmap, blur_region, trim_white_border
 
 
 def make_test_pixmap(width=100, height=80):
@@ -30,3 +30,22 @@ def test_blur_region_keeps_size_and_is_not_null(qapp):
     blurred = blur_region(pm, QRectF(20, 20, 40, 30), radius=5.0)
     assert not blurred.isNull()
     assert blurred.size() == pm.size()
+def test_trim_white_border_finds_non_white_content(qapp):
+    pm = make_test_pixmap(20, 16)
+    image = pm.toImage()
+    for y in range(4, 12):
+        for x in range(5, 15):
+            image.setPixelColor(x, y, QColor("black"))
+    pm = QPixmap.fromImage(image)
+
+    rect = trim_white_border(pm)
+
+    assert rect.x() == 5
+    assert rect.y() == 4
+    assert rect.width() == 10
+    assert rect.height() == 8
+
+
+def test_trim_white_border_keeps_all_white_image(qapp):
+    pm = make_test_pixmap(20, 16)
+    assert trim_white_border(pm) == QRectF(0, 0, 20, 16)
