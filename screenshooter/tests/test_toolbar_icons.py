e1eee8731dc201fc5431d1d97749d6f453b28b63
moleval <moleval@yandex.ru>
2026-09-24 16:54:20 +0500

    Test uniform icon-only toolbar buttons

---
+++ screenshooter/tests/test_toolbar_icons.py
@@ -0,0 +1,38 @@
+import pytest
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QAction, QApplication, QActionGroup
+
+from screenshooter.ui.annotation_toolbar import AnnotationToolbar
+from screenshooter.ui.image_toolbar import ImageToolbar
+from screenshooter.widgets.icon_manager import IconManager
+
+
+@pytest.fixture
+def qapp():
+    return QApplication.instance() or QApplication([])
+
+
+@pytest.mark.parametrize(
+    "toolbar_cls",
+    [AnnotationToolbar, ImageToolbar],
+)
+def test_toolbar_buttons_are_36px_icon_only(qapp, toolbar_cls):
+    actions = []
+    group = QActionGroup(qapp)
+    group.setExclusive(True)
+
+    for name in ("pointer", "crop"):
+        action = QAction(qapp)
+        action.setCheckable(True)
+        action.setIcon(IconManager.icon(name))
+        group.addAction(action)
+        actions.append(action)
+
+    toolbar = toolbar_cls(actions)
+    buttons = toolbar.findChildren(type(toolbar.findChildren.__self__)) if False else toolbar.findChildren(__import__("PyQt5.QtWidgets", fromlist=["QToolButton"]).QToolButton)
+
+    assert buttons
+    for button in buttons:
+        assert button.width() == 36
+        assert button.height() == 36
+        assert button.toolButtonStyle() == Qt.ToolButtonIconOnly
